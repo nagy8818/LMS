79638d0901e8f2665d61658bc7f30b2518bb3d3e
nagy khaled <121309159+nagy8818@users.noreply.github.com>
2024-03-21 18:37:12 +0200

    Update library-system.sql

--- database dump/library-system.sql
+++ database dump/library-system.sql
@@ -1,10 +1,20 @@
-
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Server version: 8.2.0
+-- PHP Version: 8.2.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `library-system`
@@ -16,11 +26,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `author`
 --
 
-CREATE TABLE `author` (
-  `authorid` int(11) NOT NULL,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `author`;
+CREATE TABLE IF NOT EXISTS `author` (
+  `authorid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`authorid`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `author`
@@ -36,32 +48,32 @@ INSERT INTO `author` (`authorid`, `name`, `status`) VALUES
 -- Table structure for table `book`
 --
 
-CREATE TABLE `book` (
-  `bookid` int(11) NOT NULL,
-  `categoryid` int(11) NOT NULL,
-  `authorid` int(11) NOT NULL,
-  `rackid` int(11) NOT NULL,
-  `name` text COLLATE utf8_unicode_ci NOT NULL,
-  `picture` varchar(250) COLLATE utf8_unicode_ci NOT NULL,
-  `publisherid` int(11) NOT NULL,
-  `isbn` varchar(30) COLLATE utf8_unicode_ci NOT NULL,
-  `no_of_copy` int(5) NOT NULL,
-  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL,
-  `added_on` datetime NOT NULL DEFAULT current_timestamp(),
-  `updated_on` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `book`;
+CREATE TABLE IF NOT EXISTS `book` (
+  `bookid` int NOT NULL AUTO_INCREMENT,
+  `categoryid` int NOT NULL,
+  `authorid` int NOT NULL,
+  `rackid` int NOT NULL,
+  `name` text CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `publisherid` int NOT NULL,
+  `isbn` varchar(30) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `no_of_copy` int NOT NULL,
+  `status` enum('Enable','Disable') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT 'Enable',
+  `added_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `picture` varchar(250) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`bookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `book`
 --
 
-INSERT INTO `book` (`bookid`, `categoryid`, `authorid`, `rackid`, `name`, `picture`, `publisherid`, `isbn`, `no_of_copy`, `status`, `added_on`, `updated_on`) VALUES
-(1, 2, 2, 2, 'The Joy of PHP Programming', 'joy-php.jpg', 8, 'B00BALXN70', 10, 'Enable', '2022-06-12 11:12:48', '2022-06-12 11:13:27'),
-(2, 2, 3, 2, 'Head First PHP &amp; MySQL', 'header-first-php.jpg', 9, '0596006306', 10, 'Enable', '2022-06-12 11:16:01', '2022-06-12 11:16:01'),
-(3, 2, 2, 1, 'dsgsdgsd', '', 7, 'sdfsd2334', 23, 'Enable', '2022-06-12 13:29:14', '2022-06-12 13:29:14'),
-(7, 1, 2, 0, 'eeeeeebook', '', 2, 'hfdfhdfhd', 2, '', '2023-03-19 16:27:17', '2023-03-19 16:27:17'),
-(8, 1, 2, 0, 'aaaaaaaaaaaaaa', '', 2, 'bbbbbbbbbbbbbbbbbb', 2, '', '2023-03-19 17:37:56', '2023-03-19 17:37:56'),
-(9, 1, 2, 1, 'bbbbbbbbbbbbbb', '', 2, '4346436463463', 2, 'Enable', '2023-03-25 14:44:18', '2023-03-25 14:44:18');
+INSERT INTO `book` (`bookid`, `categoryid`, `authorid`, `rackid`, `name`, `publisherid`, `isbn`, `no_of_copy`, `status`, `added_on`, `updated_on`, `picture`) VALUES
+(1, 2, 2, 8, 'The Joy of PHP Programming', 8, 'B00BALXN70', 10, 'Enable', '2022-06-12 11:12:48', '2022-06-12 11:13:27', 'The Joy of PHP Programming'),
+(2, 2, 3, 1, 'Head First PHP & MySQL', 9, '0596006306', 10, 'Enable', '2022-06-12 11:16:01', '2022-06-12 11:16:01', 'Head First PHP & MySQL'),
+(10, 1, 2, 1, 'random mmm', 2, '12315', 10, 'Enable', '2024-03-14 03:20:21', '2024-03-14 03:20:21', NULL),
+(11, 1, 3, 1, 'random mmm', 2, '1122', 7, 'Enable', '2024-03-14 03:20:44', '2024-03-14 03:20:44', NULL);
 
 -- --------------------------------------------------------
 
@@ -69,11 +81,13 @@ INSERT INTO `book` (`bookid`, `categoryid`, `authorid`, `rackid`, `name`, `pictu
 -- Table structure for table `category`
 --
 
-CREATE TABLE `category` (
-  `categoryid` int(11) NOT NULL,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `categoryid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`categoryid`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `category`
@@ -82,9 +96,9 @@ CREATE TABLE `category` (
 INSERT INTO `category` (`categoryid`, `name`, `status`) VALUES
 (1, 'Web Design', 'Enable'),
 (2, 'Programming', 'Enable'),
-(3, 'Commerce', 'Enable'),
 (4, 'Math', 'Enable'),
-(6, 'Web Development', 'Enable');
+(6, 'Web Development', 'Enable'),
+(7, 'random', 'Enable');
 
 -- --------------------------------------------------------
 
@@ -92,24 +106,24 @@ INSERT INTO `category` (`categoryid`, `name`, `status`) VALUES
 -- Table structure for table `issued_book`
 --
 
-CREATE TABLE `issued_book` (
-  `issuebookid` int(11) NOT NULL,
-  `bookid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `issue_date_time` datetime NOT NULL DEFAULT current_timestamp(),
+DROP TABLE IF EXISTS `issued_book`;
+CREATE TABLE IF NOT EXISTS `issued_book` (
+  `issuebookid` int NOT NULL AUTO_INCREMENT,
+  `bookid` int NOT NULL,
+  `userid` int NOT NULL,
+  `issue_date_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `expected_return_date` datetime NOT NULL,
   `return_date_time` datetime NOT NULL,
-  `status` enum('Issued','Returned','Not Return') COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+  `status` enum('Issued','Returned','Not Return') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  PRIMARY KEY (`issuebookid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `issued_book`
 --
 
 INSERT INTO `issued_book` (`issuebookid`, `bookid`, `userid`, `issue_date_time`, `expected_return_date`, `return_date_time`, `status`) VALUES
-(1, 2, 2, '2022-06-12 15:33:45', '2022-06-15 16:27:59', '2022-06-16 16:27:59', 'Not Return'),
-(3, 1, 2, '2022-06-12 18:46:07', '2022-06-30 18:46:02', '2022-06-12 18:46:14', 'Returned'),
-(4, 7, 2, '2023-03-25 14:32:57', '2023-03-25 14:32:47', '2023-03-26 14:32:51', 'Issued');
+(8, 1, 1, '2024-03-13 19:49:14', '2024-03-04 19:49:03', '2024-03-22 19:49:08', 'Issued');
 
 -- --------------------------------------------------------
 
@@ -117,11 +131,13 @@ INSERT INTO `issued_book` (`issuebookid`, `bookid`, `userid`, `issue_date_time`,
 -- Table structure for table `publisher`
 --
 
-CREATE TABLE `publisher` (
-  `publisherid` int(11) NOT NULL,
+DROP TABLE IF EXISTS `publisher`;
+CREATE TABLE IF NOT EXISTS `publisher` (
+  `publisherid` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
-  `status` enum('Enable','Disable') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `status` enum('Enable','Disable') NOT NULL,
+  PRIMARY KEY (`publisherid`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `publisher`
@@ -143,11 +159,13 @@ INSERT INTO `publisher` (`publisherid`, `name`, `status`) VALUES
 -- Table structure for table `rack`
 --
 
-CREATE TABLE `rack` (
-  `rackid` int(11) NOT NULL,
-  `name` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `status` enum('Enable','Disable') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'Enable'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+DROP TABLE IF EXISTS `rack`;
+CREATE TABLE IF NOT EXISTS `rack` (
+  `rackid` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL,
+  `status` enum('Enable','Disable') CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci NOT NULL DEFAULT 'Enable',
+  PRIMARY KEY (`rackid`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `rack`
@@ -155,7 +173,7 @@ CREATE TABLE `rack` (
 
 INSERT INTO `rack` (`rackid`, `name`, `status`) VALUES
 (1, 'R1', 'Enable'),
-(2, 'R2', 'Enable');
+(8, 'R2', 'Enable');
 
 -- --------------------------------------------------------
 
@@ -163,115 +181,50 @@ INSERT INTO `rack` (`rackid`, `name`, `status`) VALUES
 -- Table structure for table `user`
 --
 
-CREATE TABLE `user` (
-  `id` int(11) UNSIGNED NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
   `first_name` varchar(255) DEFAULT NULL,
   `last_name` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `password` varchar(64) NOT NULL,
-  `role` enum('admin','user') DEFAULT 'admin'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `email` text,
+  `role` enum('staff','user') DEFAULT 'user',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
-(2, 'Mark', 'Wood', 'mark@webdamn.com', '123', 'user'),
-(3, 'George', 'Smith', 'goerge@webdamn.com', '123', 'admin'),
-(4, 'Adam', NULL, 'adam@webdamn.com', '123', 'admin'),
-(6, 'aaa', 'bbbbb', 'ab@webdamn.com', '123', 'user');
+INSERT INTO `user` (`id`, `first_name`, `last_name`, `email`, `role`) VALUES
+(1, 'Mark', 'Wood', 'mark@webdamn.com', 'user'),
+(5, 'nas', 'asccs', 'mark@webdamn.cm', 'user');
+
+-- --------------------------------------------------------
 
 --
--- Indexes for dumped tables
+-- Table structure for table `users`
 --
 
---
--- Indexes for table `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`authorid`);
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int UNSIGNED NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(64) NOT NULL,
+  `role` enum('admin','user') DEFAULT 'admin',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
 
 --
--- Indexes for table `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`bookid`);
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
-  ADD PRIMARY KEY (`categoryid`);
-
---
--- Indexes for table `issued_book`
---
-ALTER TABLE `issued_book`
-  ADD PRIMARY KEY (`issuebookid`);
-
---
--- Indexes for table `publisher`
---
-ALTER TABLE `publisher`
-  ADD PRIMARY KEY (`publisherid`);
-
---
--- Indexes for table `rack`
---
-ALTER TABLE `rack`
-  ADD PRIMARY KEY (`rackid`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
+-- Dumping data for table `users`
 --
 
---
--- AUTO_INCREMENT for table `author`
---
-ALTER TABLE `author`
-  MODIFY `authorid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `book`
---
-ALTER TABLE `book`
-  MODIFY `bookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-  MODIFY `categoryid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
-
---
--- AUTO_INCREMENT for table `issued_book`
---
-ALTER TABLE `issued_book`
-  MODIFY `issuebookid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `publisher`
---
-ALTER TABLE `publisher`
-  MODIFY `publisherid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
-
---
--- AUTO_INCREMENT for table `rack`
---
-ALTER TABLE `rack`
-  MODIFY `rackid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+INSERT INTO `users` (`id`, `first_name`, `last_name`, `email`, `password`, `role`) VALUES
+(1, 'nagy', 'khaled', 'nagy@gmail.com', '1234', 'admin'),
+(2, 'user', 'admin', 'user@email.com', '1234', 'admin');
 COMMIT;
 
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
